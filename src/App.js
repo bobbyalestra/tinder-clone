4c379eb640ca1b3a6942891abf51872dab8d733b
Bobby Alestra <bobbyalestra135@gmail.com>
2021-06-22 08:06:07 -0400

    header for tinder clone

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
+
+import React from "react";
 import './App.css';
+import Header from "./Header"
+
 
 function App() {
   return (
     <div className="app">
-     <h1>Tinder Clone</h1>
+    
 
-
+<Header />
     {/* {header} */}
     {/*Tindercards */}
     </div>
--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import "./Header.css";
+import PersonIcon from "@material-ui/icons/Person"
+import { IconButton } from '@material-ui/core';
+import ()
+
 
 function Header() {
     return (
         <div className='header'>
-            
+            <h2> Header</h2>
+            <IconButton>
+            <PersonIcon fontSize="large" className="header-icon" />
+            </IconButton>
+           
+           <img
+               className="header__logo"
+               src="https://1000logos.net/wp-content/uploads/2018/07/tinder-emblem-768x432.jpg"
+               alt=""
+           />
+
+
+
+
         </div>
     )
 }
